cea76c3489aef5965387c6b0a8ec1c290d29e4d0
Marco Maldonado <loweaxerium@gmail.com>
2017-01-04 15:14:51 -0600

    Basic working setup

---
+++ assistance/server.js
@@ -0,0 +1,37 @@
+var http = require('http')
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, true)
+  var date = new Date(parsedUrl.query.iso)
+  var time;
+  switch (parsedUrl.pathname) {
+    case '/api/parsetime':
+    time = parsetime(date)
+    break;
+    case '/api/unixtime':
+    time = unixtime(date)
+    break;
+    default:
+  }
+  if (time) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(time))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+})
+server.listen(5000)
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
